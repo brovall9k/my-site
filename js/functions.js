fddbf882194e2e111334198236d75e3aa2a8f7a5
Brandon Stovall <stovallxbrandon@gmail.com>
2015-09-23 02:20:01 -0500

    continued work on the frontend
    reorganized how the sidebar-item and header-item gain and lose focus.
    plans have been established to create the functions necessary for the
    sidebar.

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
 
+
+
 });
+
+/*
+when the topmost sidebar-item reaches the top
+of the sidebar-container...the position of the
+sidebar-items becomes fixed at ( top: 0; )
+*/
+
+/*
+whenever a $(".sidebar-item") is clicked the
+$(.sidebar-item-focused) class is toggeled "off"
+the currently "focused" item and toggels the
+class "on" for the newly clicked $(".sidebar-item")
+*/
